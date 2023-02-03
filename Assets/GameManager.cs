32c0d6051c20d3add8181ef26a5525344f9e9166
borb <perttu.tuovinen@buutti.com>
2023-02-03 21:33:39 +0200

    add gameover & gamemanager singleton

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
