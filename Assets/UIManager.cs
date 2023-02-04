4f875ffa70f67f8438ee31546c497ec4a1acbae0
borb <perttu.tuovinen@buutti.com>
2023-02-04 19:37:10 +0200

    add Enemy tag, add lava prefab, add hp

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    GameObject rareCollectiblesObj;
     TextMeshProUGUI rareCollectiblesText;
+    TextMeshProUGUI hpText;
     void Start()
     {
-        rareCollectiblesObj = transform.Find("RareCollectibles").gameObject;
+        GameObject rareCollectiblesObj = transform.Find("RareCollectibles").gameObject;
         rareCollectiblesText = rareCollectiblesObj.GetComponent<TextMeshProUGUI>();
+
+        GameObject hpObj = transform.Find("HP").gameObject;
+        hpText = hpObj.GetComponent<TextMeshProUGUI>();
+
         GameManager.instance.uiManager = this;
     }
     public void SetRareCollectibles(int newScore)
     {
         rareCollectiblesText.text = newScore.ToString();
+    }
+
+    public void SetHP(int newHP)
+    {
+        hpText.text = newHP.ToString();
     }
 }
